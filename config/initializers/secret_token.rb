0e37796ad4f74b0bcc2dc0069765472921bf8075
Mark Wilkinson <markawil@yahoo.com>
2012-04-26 12:19:49 -0500

    Initial commit after starting the project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PacManRails::Application.config.secret_token = 'ba02034107231717edd2b754c90a34cab20810db36836c884f406733f037b5b598cfd294fa7dd45b269482fb385156a2fb41e6d91d6d3313989883943d8c66a9'
